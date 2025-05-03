c814e1cee1a8f02c3a32ddf98dba5f3d91fa31cd
raicse21 <rishikeshrai212012@gmai.com>
2025-05-03 14:49:54 +0530

    Completed the Project with API Gateway & Service Registry

--- QuizService/src/main/java/com/quiz/entities/Question.java
+++ QuizService/src/main/java/com/quiz/entities/Question.java
@@ -1,10 +1,6 @@
 package com.quiz.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
